ec0c7f3c753c540dd0ddc471786b83c0ffd51f89
Harshit <librahtiwari@gmail.com>
2018-04-26 20:32:01 +0530

    added script for parsing of XML files

---
+++ extract_features_using_text.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import lxml
+import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+
